1992c125a0eb6fa86e4702f041a188274ff50929
hamelsmu <hamel.husain@gmail.com>
2022-02-27 21:46:15 -0800

    save

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'hamelsmu', // Usually your GitHub org/user name.
   projectName: 'notes', // Usually your repo name.
   plugins: [],
   presets: [
