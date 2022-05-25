36ba605ef480982e88cf74b89da65f19c5f84c48
hamelsmu <hamel.husain@gmail.com>
2022-05-25 11:04:02 -0700

    update docs

---
+++ docs/myflow.py
@@ -0,0 +1,21 @@
+
+from metaflow import FlowSpec, step
+
+
+class MyFlow(FlowSpec):
+
+    @step
+    def start(self):
+        self.next(self.middle)
+
+    @step
+    def middle(self):
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+
+if __name__ == '__main__':
+    MyFlow()
